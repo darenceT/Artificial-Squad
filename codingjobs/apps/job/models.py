410843691f805e1d8a0c72d9578e879abd297b22
darenceT <78669396+darenceT@users.noreply.github.com>
2021-09-03 18:33:23 -0700

    jobs additions

--- codingjobs/apps/job/models.py
+++ codingjobs/apps/job/models.py
@@ -2,10 +2,14 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Job(models.Model):
-	title = models.CharField(max_length=255)
-	goals_description = models.TextField()
-	skills_description = models.TextField(blank=True, null=True)
+	title = models.CharField(max_length=255) 	# will display as Title (any_how -> Any How)
+	location = models.CharField(max_length=255, blank=True, null=True)
+	description = models.TextField()
+	requirements = models.TextField(blank=True, null=True)
 
 	created_by = models.ForeignKey(User, related_name='jobs', on_delete=models.CASCADE)
 	created_at = models.DateTimeField(auto_now_add=True)
 	changed_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
