11200a8c302a0e72a85f2fc9c4b5fa673237254b
darenT <darence.thong@gmail.com@users.noreply.github.com>
2021-10-06 23:51:14 -0700

    checkbox and profile functional
    Checkboxes for skills in profiles now saves and can be viewed on profile
    Next:
    -improve look of skills on profile.html
    -only shows 1st skill? need to fix
    -apply for job broken, form does not save
    -now can start working on AI linking by:
    --add same skils checkbox for jobs

--- apps/userprofile/forms.py
+++ apps/userprofile/forms.py
@@ -5,9 +5,6 @@ from .models import User_profile
 class Profile_form(forms.ModelForm):
 	class Meta:
 		model = User_profile
-		fields = ('full_name', 'relevant_background', 'values', 'career_goals')
-	product_management = forms.BooleanField(required=False)
-	web_developement = forms.BooleanField(required=False)
-	user_experience = forms.BooleanField(required=False)
-	marketing = forms.BooleanField(required=False)
-	finance = forms.BooleanField(required=False)
+		fields = ('full_name', 'relevant_background', 'values', 'career_goals',
+				'product_management', 'web_developement', 'user_experience',
+				'marketing', 'finance')
